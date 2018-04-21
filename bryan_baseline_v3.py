75555946af47ee7baf2af40372926fade3a59b91
yaozzhou(周耀) <yaozzhou@tencent.com>
2018-04-22 00:38:01 +0800

    add v3

---
+++ bryan_baseline_v3.py
@@ -0,0 +1,117 @@
+# coding=utf-8
+# @author:bryan
+# blog: https://blog.csdn.net/bryan__
+# github: https://github.com/YouChouNoBB/2018-tencent-ad-competition-baseline
+import pandas as pd
+import lightgbm as lgb
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder
+from scipy import sparse
+import os
+import gc
+import math
+import numpy as np
+
+ad_feature=pd.read_csv('../data/adFeature.csv')
+
+def get_user_feature():
+    if os.path.exists('../data/userFeature.csv'):
+        user_feature=pd.read_csv('../data/userFeature.csv')
+    else:
+        userFeature_data = []
+        with open('../data/userFeature.data', 'r') as f:
+            for i, line in enumerate(f):
+                line = line.strip().split('|')
+                userFeature_dict = {}
+                for each in line:
+                    each_list = each.split(' ')
+                    userFeature_dict[each_list[0]] = ' '.join(each_list[1:])
+                userFeature_data.append(userFeature_dict)
+                if i % 100000 == 0:
+                    print(i)
+            user_feature = pd.DataFrame(userFeature_data)
+            user_feature.to_csv('../data/userFeature.csv', index=False)
+        gc.collect()
+    return user_feature
+train=pd.read_csv('../data/train.csv')
+predict=pd.read_csv('../data/test1.csv')
+train.loc[train['label']==-1,'label']=0
+predict['label']=-1
+
+def batch_predict(slice,index):
+    user_feature=get_user_feature()
+    data=pd.concat([slice,predict])
+    data=pd.merge(data,ad_feature,on='aid',how='left')
+    data=pd.merge(data,user_feature,on='uid',how='left')
+    data=data.fillna('-1')
+    del user_feature,slice
+    one_hot_feature=['LBS','age','carrier','consumptionAbility','education','gender','house','os','ct','marriageStatus','advertiserId','campaignId', 'creativeId',
+           'adCategoryId', 'productId', 'productType']
+    vector_feature=['appIdAction','appIdInstall','interest1','interest2','interest3','interest4','interest5','kw1','kw2','kw3','topic1','topic2','topic3']
+    for feature in one_hot_feature:
+        try:
+            data[feature] = LabelEncoder().fit_transform(data[feature].apply(int))
+        except:
+            data[feature] = LabelEncoder().fit_transform(data[feature])
+
+    train=data[data.label!=-1]
+    train_y=train.pop('label')
+    test=data[data.label==-1]
+    res=test[['aid','uid']]
+    test=test.drop('label',axis=1)
+    enc = OneHotEncoder()
+    train_x=train[['creativeSize']]
+    test_x=test[['creativeSize']]
+
+    for feature in one_hot_feature:
+        enc.fit(data[feature].values.reshape(-1, 1))
+        train_a=enc.transform(train[feature].values.reshape(-1, 1))
+        test_a = enc.transform(test[feature].values.reshape(-1, 1))
+        train_x= sparse.hstack((train_x, train_a))
+        test_x = sparse.hstack((test_x, test_a))
+        print(feature+' finish')
+    print('one-hot prepared !')
+
+    cv=CountVectorizer()
+    for feature in vector_feature:
+        cv.fit(data[feature])
+        train_a = cv.transform(train[feature])
+        test_a = cv.transform(test[feature])
+        train_x = sparse.hstack((train_x, train_a))
+        test_x = sparse.hstack((test_x, test_a))
+        print(feature + ' finish')
+    print('cv prepared !')
+    del data
+    return LGB_predict(train_x, train_y, test_x, res,index)
+
+def LGB_predict(train_x,train_y,test_x,res,index):
+    print("LGB test")
+    clf = lgb.LGBMClassifier(
+        boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
+        max_depth=-1, n_estimators=1500, objective='binary',
+        subsample=0.7, colsample_bytree=0.7, subsample_freq=1,
+        learning_rate=0.05, min_child_weight=50, random_state=2018, n_jobs=-1
+    )
+    clf.fit(train_x, train_y, eval_set=[(train_x, train_y)], eval_metric='auc',early_stopping_rounds=100)
+    res['score'+str(index)] = clf.predict_proba(test_x)[:,1]
+    res['score'+str(index)] = res['score'+str(index)].apply(lambda x: float('%.6f' % x))
+    print(str(index)+' predict finish!')
+    gc.collect()
+    return res['score'+str(index)]
+
+#数据分片处理，对每片分别训练预测，然后求平均
+cnt=20
+size = math.ceil(len(train) / cnt)
+result=[]
+for i in range(cnt):
+    start = size * i
+    end = (i + 1) * size if (i + 1) * size < len(train) else len(train)
+    slice = train[start:end]
+    result.append(batch_predict(slice,i))
+    gc.collect()
+
+result=pd.concat(result,axis=1)
+result['score']=np.mean(result,axis=1)
+result=pd.concat([predict['aid','uid'],result['score']],axis=1)
+result[['aid','uid','score']].to_csv('../data/submission.csv', index=False)
+os.system('zip ../data/baseline.zip ../data/submission.csv')
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 - 2018腾讯广告大赛baseline 100行代码带你上0.73
 ![Image text](https://github.com/YouChouNoBB/2018-tencent-ad-competition-baseline/blob/master/pic/leadboard.jpg)
 
-- baseline
+- baseline  在内存大于32g的情况下使用，挑整参数，可以上0.74
 - 1.首先处理4个G的用户特征
 - 2.拼接用户特征，广告特征
 - 3.将单取值的离散特征使用稀疏方式one-hot
@@ -11,6 +11,7 @@
 - 5.线下测试
 - 6.线上提交
 
-- baseline_v2
+##
+- baseline_v2  16g左右内存使用，建议开启虚拟内存
 - 将训练集分片，分别训练预测，然后将预测结果求平均，大概需要15g内存
 - 如果还觉得内存占用过多，可以尝试每次join完之后del 掉用户特征，下次训练再重新读取，这样应该8g内存就能跑了
