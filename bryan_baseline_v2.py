28a3384e832f4bc0e23377ea08f9d61959c1f8fe
yaozzhou(周耀) <yaozzhou@tencent.com>
2018-04-30 21:02:02 +0800

    fix

--- bryan_baseline_v2.py
+++ bryan_baseline_v2.py
@@ -20,28 +20,30 @@ predict['label']=-1
 if os.path.exists('../data/userFeature.csv'):
     user_feature=pd.read_csv('../data/userFeature.csv')
 else:
-    cnt = 0
-    for i, line in enumerate(f):
-        line = line.strip().split('|')
-        userFeature_dict = {}
-        for each in line:
-            each_list = each.split(' ')
-            userFeature_dict[each_list[0]] = ' '.join(each_list[1:])
-        userFeature_data.append(userFeature_dict)
-        if i % 100000 == 0:
-            print(i)
-        if i % 1000000 == 0:
-            user_feature = pd.DataFrame(userFeature_data)
-            user_feature.to_csv('../data/userFeature_' + str(cnt) + '.csv', index=False)
-            cnt += 1
-            del userFeature_data, user_feature
-            userFeature_data = []
-    user_feature = pd.DataFrame(userFeature_data)
-    user_feature.to_csv('../data/userFeature_' + str(cnt) + '.csv', index=False)
-    del userFeature_data, user_feature
-    user_feature = pd.concat(
-        [pd.read_csv('../data/userFeature_' + str(i) + '.csv') for i in range(cnt + 1)]).reset_index(drop=True)
-    user_feature.to_csv('../data/userFeature.csv', index=False)
+    userFeature_data = []
+    with open('../data/userFeature.data', 'r') as f:
+        cnt = 0
+        for i, line in enumerate(f):
+            line = line.strip().split('|')
+            userFeature_dict = {}
+            for each in line:
+                each_list = each.split(' ')
+                userFeature_dict[each_list[0]] = ' '.join(each_list[1:])
+            userFeature_data.append(userFeature_dict)
+            if i % 100000 == 0:
+                print(i)
+            if i % 1000000 == 0:
+                user_feature = pd.DataFrame(userFeature_data)
+                user_feature.to_csv('../data/userFeature_' + str(cnt) + '.csv', index=False)
+                cnt += 1
+                del userFeature_data, user_feature
+                userFeature_data = []
+        user_feature = pd.DataFrame(userFeature_data)
+        user_feature.to_csv('../data/userFeature_' + str(cnt) + '.csv', index=False)
+        del userFeature_data, user_feature
+        user_feature = pd.concat(
+            [pd.read_csv('../data/userFeature_' + str(i) + '.csv') for i in range(cnt + 1)]).reset_index(drop=True)
+        user_feature.to_csv('../data/userFeature.csv', index=False)
 
 
 def LGB_predict(train_x,train_y,test_x,res,index):
